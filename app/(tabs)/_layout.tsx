7ec322c95bb7cebf14bae6f94a9f0a8e2c32aedb
joaozinho2905 <joao.anhaia.santos@escola.pr.gov.br>
2025-09-10 12:09:06 -0300

    separação de páginas

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -33,6 +33,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
+            <Tabs.Screen
+        name="termos"
+        options={{
+          title: '333',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
       <Tabs.Screen
         name="explore"
         options={{
