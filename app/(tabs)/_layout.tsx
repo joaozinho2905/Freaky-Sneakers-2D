9f44acd1b3a73ee8c16c033267c7d263048d5e16
luizhenrickoliveira <luiz.oliveira.matos@escola.pr.gov.br>
2025-10-01 12:15:20 -0300

    Arrumando botões

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,14 +29,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Página Inicial',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
             <Tabs.Screen
         name="termos"
         options={{
-          title: '333',
+          title: 'Termos de Uso',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
