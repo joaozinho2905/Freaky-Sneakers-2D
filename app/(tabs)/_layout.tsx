08d9c63c8539c1d2cfb1dce1631a24b32f2a3843
luizhenrickoliveira <luiz.oliveira.matos@escola.pr.gov.br>
2025-10-29 12:23:11 -0300

    Adicionando símbolos

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,6 +7,13 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Entypo from '@expo/vector-icons/Entypo';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -20,7 +27,6 @@ export default function TabLayout() {
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
@@ -29,22 +35,99 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Página Inicial',
+          tabBarIcon: ({ color }) => <IconSymbol size={18} name="home" color="pink" />,
         }}
       />
             <Tabs.Screen
         name="termos"
         options={{
-          title: '333',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Termos de uso',
+          tabBarIcon: ({ color }) => <IconSymbol size={18} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="contato"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Contato',
+          tabBarIcon: ({ color }) => <AntDesign name="contacts" size={18} color="gray" />,
+        }}
+      />
+      <Tabs.Screen
+        name="faq"
+        options={{
+          title: 'Perguntas Frequentes',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="frequently-asked-questions" size={18} color="gray" />,
+        }}
+      />
+        <Tabs.Screen
+        name="cadastro"
+        options={{
+          title: 'Cadastro',
+          tabBarIcon: ({ color }) => <FontAwesome5 name="registered" size={18} color="gray" />,
+        }}
+      />
+        <Tabs.Screen
+        name="sobrenos"
+        options={{
+          title: 'Sobre nós',
+          tabBarIcon: ({ color }) => <FontAwesome5 name="readme" size={18} color="gray" />,
+        }}
+      />
+        <Tabs.Screen
+        name="categorias"
+        options={{
+          title: 'Categorias',
+          tabBarIcon: ({ color }) => <MaterialIcons name="category" size={18} color="gray" />,
+        }}
+      />
+        <Tabs.Screen
+        name="nike"
+        options={{
+          title: 'Nike',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="shoe-sneaker" size={18} color="gray" />,
+        }}
+      />
+          <Tabs.Screen
+        name="puma"
+        options={{
+          title: 'Puma',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="shoe-sneaker" size={18} color="gray" />,
+        }}
+      />
+          <Tabs.Screen
+        name="adidas"
+        options={{
+          title: 'Adidas',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="shoe-sneaker" size={18} color="gray" />,
+        }}
+      />
+         <Tabs.Screen
+        name="login"
+        options={{
+          title: 'Login',
+          tabBarIcon: ({ color }) => <Entypo name="login" size={18} color="gray" />,
+        }}
+      />
+            <Tabs.Screen
+        name="painelcliente"
+        options={{
+          title: 'Painel do Cliente',
+          tabBarIcon: ({ color }) => <FontAwesome name="dashcube" size={18} color="gray" />,
+        }}
+      />
+                 <Tabs.Screen
+        name="informaçãoes"
+        options={{
+          title: 'Informações',
+          tabBarIcon: ({ color }) => <Ionicons name="information" size={18} color="gray" />,
+        }}
+      />
+                     <Tabs.Screen
+        name="politicaprivacidade"
+        options={{
+          title: 'Política de Privacidade',
+          tabBarIcon: ({ color }) => <MaterialIcons name="policy" size={18} color="gray" />,
         }}
       />
     </Tabs>
